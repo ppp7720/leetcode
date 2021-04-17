a73b2a356e4decc00dfa521d81d8e36d148c9ade
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-18 03:59:10 +0900

    Add files via upload

--- 118. Pascal's Triangle(E).py
+++ 118. Pascal's Triangle(E).py
@@ -1,11 +1,8 @@
 numRows = 5
 
-
-n = [[1]]
-i = 0
-while i <= 5:
-    n.append([a+b for a,b in zip(n[-1]+[0], [0]+n[-1])])
-    i += 1
-
-print(n)
-
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = [[1]]
+        for i in range(numRows-1):
+            rows.append([a+b for a,b in zip(rows[-1]+[0], [0]+rows[-1])])
+        return rows
