556022b906d2cd609479f27f83902b1ff484220f
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-18 23:56:06 +0900

    Add files via upload

---
+++ 167. Two Sum II - Input array is sorted(E).py
@@ -0,0 +1,11 @@
+numbers = [-1,0]
+target = -1
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+        while (i<j):
+            if numbers[i]+numbers[j] == target: return [i+1,j+1]
+            elif numbers[i]+numbers[j] < target: i += 1
+            else: j -= 1
