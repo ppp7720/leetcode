284d36eb117769b98919fb4d9699f489f3f465fd
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-19 20:34:15 +0900

    Add files via upload

---
+++ 171. Excel Sheet Column Number(E).py
@@ -0,0 +1,7 @@
+columnTitle = "AAAAAA"
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for i in columnTitle: result = (result * 26) + (ord(i) - 64)
+        return result
