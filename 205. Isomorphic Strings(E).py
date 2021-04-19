891aeef8704244f7a6d79e55771fdad803c34b2c
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-19 23:33:29 +0900

    Add files via upload

---
+++ 205. Isomorphic Strings(E).py
@@ -0,0 +1,10 @@
+s = "paper"
+t = "title"
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        for i in range(len(s)):
+            if s.find(s[i]) != t.find(t[i]): return False
+        
+        return True
