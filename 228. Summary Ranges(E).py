ce4cc35d385107435f0851905044f7fce6135860
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-04-20 20:27:26 +0900

    Update 228. Summary Ranges(E).py

--- 228. Summary Ranges(E).py
+++ 228. Summary Ranges(E).py
@@ -4,10 +4,8 @@ class Solution:
         a = ""
         for i, n in enumerate(nums):
             if a == "": a = n
-            try:
-                if i == len(nums)-1 or nums[i+1] - n > 1:
-                    if a == n: r.append(str(a))
-                    else: r.append(str(a)+"->"+str(n))
-                    a = ""
-            except: continue
+            if i == len(nums)-1 or nums[i+1] - n > 1:
+                if a == n: r.append(str(a))
+                else: r.append(str(a)+"->"+str(n))
+                a = ""
         return r
