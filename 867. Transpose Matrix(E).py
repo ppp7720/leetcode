bdc7f89e969169f8a3ff1ee5c55594399d58fb00
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-05-04 01:45:07 +0900

    Update 867. Transpose Matrix(E).py

--- 867. Transpose Matrix(E).py
+++ 867. Transpose Matrix(E).py
@@ -1,8 +1,13 @@
 matrix = [[1,2,3],[4,5,6]]
 
-newmat = [[] for _ in range(len(matrix[0]))]
-for c in range(len(matrix[0])):
-    for r in range(len(matrix)):
-        newmat[c].append(matrix[r][c])
-        
-print(newmat)
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        newmat = []
+        for c in range(len(matrix[0])):
+            newmat.append([])
+            for r in range(len(matrix)):
+                newmat[c].append(matrix[r][c])
+        return newmat
+
+    def transpose2(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [[matrix[r][c] for r in range(len(matrix))] for c in range(len(matrix[0]))]
