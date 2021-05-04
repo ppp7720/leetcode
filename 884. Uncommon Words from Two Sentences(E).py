41c90629f969303b89c6b61f7e505ba6a1d523b1
ppp7720 <51737405+ppp7720@users.noreply.github.com>
2021-05-04 15:32:12 +0900

    Update 884. Uncommon Words from Two Sentences(E).py

--- 884. Uncommon Words from Two Sentences(E).py
+++ 884. Uncommon Words from Two Sentences(E).py
@@ -1,7 +1,9 @@
 A = "this apple is sweet"
 B = "this apple is sour"
 
-S = A.split() + B.split()
-h = dict()
-for i in S: h[i] = h.get(i,0) + 1
-print([s for s,c in h.items() if c == 1])
+class Solution:
+    def uncommonFromSentences(self, A: str, B: str) -> List[str]:
+        S = A.split() + B.split()
+        h = dict()
+        for i in S: h[i] = h.get(i,0) + 1
+        return [s for s,c in h.items() if c == 1]
